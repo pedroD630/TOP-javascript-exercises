750ba48dd7b1ec7b4276dc588cb7ccf4623851a7
pedro <87133537+pedroD630@users.noreply.github.com>
2024-01-02 16:23:03 -0300

    Soluciona exercicio 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+// receber uma string como parametro
+// rodar a string com um loop de traz pra frente como um array
+// adicionar cada elemento de traz pra frente em uma nova string
+    let textLenght = text.length;
+    let reversed = "";
+    
+    for (let i = textLenght - 1; i >= 0; i--){
+        reversed += text[i];
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
