78baa116ae34b27fe78c5cc6817dcb5a67265130
pedro <87133537+pedroD630@users.noreply.github.com>
2024-01-05 07:42:02 -0300

    Remove multiplos valores

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,43 +1,78 @@
-function findIndex(array, value){
-    let index = array.indexOf(value);
+function findIndex(array, value) {
+    console.log("Array: ", array);
+    console.log("Value: ", value);
+    let index = [];
+
+    let listLenght = array.length;
+    let deleteLen = value.length;
+    let j = 0;
+    for (let i = 0; i < deleteLen; i++) {
+        index[j] = array.indexOf(value[i]);
+        j++;
+    }
+    console.log("RemoveIndex: ", index);
     return index;
 }
 
-function removeValue(array, value) {
-    let removeIndex = findIndex(array, value);
-    let removed = array.splice(removeIndex, 1);
+function removeValues(array, values) {
+    let removeIndex = findIndex(array, values);
+    let lenght = removeIndex.length;
+
+    let removed = [];
+    for (let i = lenght - 1; i >= 0; i--) {
+        removed[i] = array.splice(removeIndex[i], 1);
+    }
+    console.log("Removed: ", removed);
     return array;
 }
 
-function findValue(array, value) {
+function findValue(array, values) {
     let length = array.length;
+    let numArgs = values.length;
+    let onList = [];
+
     for (let i = 0; i < length; i++) {
-        if (array[i] == value) {
-            return true;
+        for (let j = 0; j < numArgs; j++) {
+            if (array[i] == values[j]) {
+                onList[i] = true;
+            }
+        }
+        if (onList[i] == undefined) {
+            onList[i] = false;
         }
     }
-    return false;
+    console.log("OnList: ", onList);
+    return onList;
 }
 
-const removeFromArray = function (array, remove) {
+const removeFromArray = function (array, ...args) {
     // recebe um array e os argumentos para remover
-    // tratar argumentos opcionais
-   
+    // tratar argumentos opcionais rest sintax
+
     let newArray = [];
+    let lenght = array.length;
 
-     // checar se o elemento presente num argumento esta no array 
-    let onList = findValue(array, remove);
-    if (onList == true) {
-        // se um dos elementos estiver presente, remover ele
-        newArray = removeValue(array, remove);
+    // checar se os elementos presentes num argumento esta no array 
+    let onList = findValue(array, args);
+    let remove = [];
+
+    let j = 0;
+    for (let i = 0; i < lenght; i++) {
+        if (onList[i] == true) {
+            remove[j] = array[i];
+            console.log("Remove: ", remove[j]);
+            j++;
+        }
     }
-
+    console.log("Remove: ", remove);
+    newArray = removeValues(array, remove);
     // remove apenas elementos de mesmo tipo 1 != "1"
 
-     // retornar array sem os elementos especificados
+    // retornar array sem os elementos especificados
+    console.log("Remove: ", remove);
+    console.log("NewArray: ", newArray);
     return newArray;
-   
-};
 
+};
 // Do not edit below this line
 module.exports = removeFromArray;
