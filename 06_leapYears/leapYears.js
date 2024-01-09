f41ab503d268312f57c0ec7e2f98bf4db032f1d1
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-01-09 17:22:48 -0300

    Descobre se um ano é bissexto

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // determinar se um ano é bissexto
+    // anos bissextos são divisiveis por quatro
+    // mesmo se for divisivel por 4, se for divisivel por 100 mas não por 400 não é bissexto
 
+    let isLeap = false;
+
+    if (year % 4 == 0) {
+        isLeap = true;
+    }
+    if (isLeap && year % 100 == 0 && year % 400 != 0) {
+        isLeap = false;
+    }
+
+    return isLeap;
 };
 
 // Do not edit below this line
