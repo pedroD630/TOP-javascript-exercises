fb6be7ca7a18677e977a442590dfcaba18542f07
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-01-09 17:38:40 -0300

    Converte celsius-fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+
+  let celsius = (temp - 32) * (5 / 9);
+
+  celsius = celsius.toFixed(1);
+  celsius = parseFloat(celsius);
+
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahr = (temp * 9 / 5) + 32;
+
+  fahr = fahr.toFixed(1);
+  fahr = parseFloat(fahr);
+
+  return fahr;
 };
 
 // Do not edit below this line
