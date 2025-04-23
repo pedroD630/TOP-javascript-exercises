b1010ca00304e23c23056e196bd60d43288403d1
PedroWin <87133537+pedroD630@users.noreply.github.com>
2025-04-23 17:16:26 -0300

    solve exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	if (values.length === 0) return 0;
+
+  var sum = values.reduce((total, current) =>{
+    return total + current
+  }, 0)
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(values) {
+  if (values.length === 0) return 0;
 
+  var mult = values.reduce((total, current) =>{
+    return total * current
+  }, 1)
+
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  var total = base;
+	for(var i = 1; i < power; i++){
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function calcFactorial(start) {
+	if (start == 1 || start == 0) return 1;
+
+  var total = start * (calcFactorial(start-1));
+  return total;
 };
 
 // Do not edit below this line
