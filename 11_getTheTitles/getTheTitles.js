e008d9e06b19042860eb09fb65aa87169ce38619
PedroWin <87133537+pedroD630@users.noreply.github.com>
2025-04-24 17:25:07 -0300

    Solve exercise 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    var length = books.length;
+    var titles = [];
+    for (var i = 0; i < length; i++){
+        titles.push(books[i].title);
+    }
 
+    return titles;
 };
 
 // Do not edit below this line
