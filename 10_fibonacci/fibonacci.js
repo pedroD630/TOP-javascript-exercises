9b3929b0212014a10ad0a56865c3920c7c9f5cc7
PedroWin <87133537+pedroD630@users.noreply.github.com>
2025-04-24 15:29:39 -0300

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(memberPos) {
+    //memberPos is the index that contain the item in this implementation
 
+    if (memberPos == 0) return 0;
+    if (memberPos == 1) return 1;
+    if (Math.sign(memberPos) == -1) return 'OOPS';
+
+    var fibonacci = [0, 1];
+    for(var i = 2; i <= memberPos; i++){
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    } 
+    
+    return fibonacci[memberPos];
 };
 
 // Do not edit below this line
