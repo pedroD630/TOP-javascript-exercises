f79938475ff0387e24a965836fb59ee508e17ab8
PedroWin <87133537+pedroD630@users.noreply.github.com>
2025-04-24 14:40:19 -0300

    Solve exercise 8_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+function isSpecialCharacter(char) {
+    return /[a-zA-Z0-9]/.test(char);
+}
 
+function reverseText(cleanText){
+    var reversed = [];
+    for(var i = cleanText.length - 1; i >= 0; i--){
+        reversed.push(cleanText[i]);
+    }
+    return reversed;
+}
+
+const palindromes = function (text) {
+    var textArray = text.toLowerCase().split("");
+    var cleanText = textArray.filter(isSpecialCharacter);
+
+    var reversedText = reverseText(cleanText).join("");
+    var cleanedOriginal = cleanText.join("");
+    return reversedText === cleanedOriginal
 };
 
 // Do not edit below this line
